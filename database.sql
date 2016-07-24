420ff3c1fea62a2d67151b10868ec76650fff19b
Renaud Theuillon <rtheuillon@hotmail.com>
2016-07-24 17:15:19 +0200

    Commit

--- database.sql
+++ database.sql
@@ -14,6 +14,25 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: daf; Type: DATABASE; Schema: -; Owner: admin
+--
+
+CREATE DATABASE daf WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'French_France.1252' LC_CTYPE = 'French_France.1252';
+
+
+ALTER DATABASE daf OWNER TO admin;
+
+\connect daf
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
